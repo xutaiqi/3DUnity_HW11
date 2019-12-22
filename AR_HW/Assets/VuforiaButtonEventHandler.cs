5b107ab5d3c3866152fb56ceac9852876eba4e96
xutaiqi <869307087@qq.com>
2019-12-22 14:56:26 +0800

    HW

---
+++ AR_HW/Assets/VuforiaButtonEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
+using System;
+using UnityEngine;
+
+public class VuforiaButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    // Use this for initialization
+    public VirtualButtonBehaviour vb;
+    public Animator ani;
+    void Start()
+    {
+        vb.RegisterEventHandler(this);
+        Debug.Log("start");
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+
+        ani.SetTrigger("Take off");
+
+        Debug.Log("pressed");
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+        ani.SetTrigger("Land");
+        Debug.Log("released");
+    }
+}
